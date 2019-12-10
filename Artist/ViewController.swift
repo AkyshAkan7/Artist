80da62aaa0d956844691dbdf32178da5fbb62187
Akan <23879@iitu.kz>
2019-12-10 10:29:43 +0600

    Initial Commit

---
+++ Artist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artist
+//
+//  Created by Akan Akysh on 12/10/19.
+//  Copyright © 2019 Akysh Akan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
